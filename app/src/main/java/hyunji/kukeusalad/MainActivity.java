86e3f7c8e0550d3223e855f447874bac26249e30
Hyun-Ji Noel Jung <chunghj09@gmail.com>
2016-07-12 00:23:50 +0900

    갓산님 코드 리뷰 수정 완료 1. 이미지 위치 옯기기 2. Theme.AppCompat.Light.NoActionBar 로 변경 필요없는것 삭제 2.  listview personListAdapter.notifyDataSetChanged(); 현재 위치에서는 필요없으므로 삭제함 3. for문으로변경

--- app/src/main/java/hyunji/kukeusalad/MainActivity.java
+++ app/src/main/java/hyunji/kukeusalad/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.Window;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
@@ -35,36 +34,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
         kukeuPersonList = new ArrayList<>();
 
-        kukeuPersonList.add(new KukeuPerson());
-        kukeuPersonList.add(new KukeuPerson());
-        kukeuPersonList.add(new KukeuPerson());
-        kukeuPersonList.add(new KukeuPerson());
-        kukeuPersonList.add(new KukeuPerson());
-        kukeuPersonList.add(new KukeuPerson());
-        kukeuPersonList.add(new KukeuPerson());
-        kukeuPersonList.add(new KukeuPerson());
-        kukeuPersonList.add(new KukeuPerson());
-        kukeuPersonList.add(new KukeuPerson());
-        kukeuPersonList.add(new KukeuPerson());
-        kukeuPersonList.add(new KukeuPerson());
-        kukeuPersonList.add(new KukeuPerson());
-        kukeuPersonList.add(new KukeuPerson());
-        kukeuPersonList.add(new KukeuPerson());
-        kukeuPersonList.add(new KukeuPerson());
-        kukeuPersonList.add(new KukeuPerson());
+        for (int i=0; i<20 ; i++){
+
+            kukeuPersonList.add(new KukeuPerson());
+        }
 
         personListAdapter = new PersonListAdapter(kukeuPersonList);
         vRecyclerView.setAdapter(personListAdapter);
         vRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
 
 
-        personListAdapter.notifyDataSetChanged();
 
     }
 
